671d948e2927ed4237b72a04c21e1d2c59a618f3
Andrew Miller <andrew_miller@bctransit.com>
2015-04-24 15:14:54 -0700

    Added Test Template

---
+++ features/future/new_template.feature
@@ -0,0 +1,12 @@
+Feature: <Feature Title>
+
+As a <Somebody>
+I want <To do something>
+Because <The reason to do it>
+
+  Scenario: <Scenario description>
+     Given <Existing state>
+      when <Do something>
+      then <Something will happen>
+      and <Some acceptance condition will be true>
+
---
+++ features/steps/base_template.py
@@ -0,0 +1,15 @@
+@given(u'<Existing state>')
+def step_impl(context):
+    pass
+
+@when(u'<Do something>')
+def step_impl(context):
+    pass
+
+@then(u'<Something will happen>')
+def step_impl(context):
+    pass
+
+@then(u'<Some acceptance condition will be true>')
+def step_impl(context):
+    pass
